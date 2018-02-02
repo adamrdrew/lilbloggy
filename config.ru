5532a6642de88b218fb5e87687078d35ece13c8a
Adam Drew <adamrdrew@live.com>
2018-02-01 19:33:10 -0500

    Scaffolding

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'vueck'
+require './test'
+require 'pry'
+
+use VueCK::Plugin
+run LilBloggy
---
+++ lilbloggy.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'slim'
+
+
+class LilBloggy < Sinatra::Base
+
+    get "/" do
+        
+    end
+
+end
