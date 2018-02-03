4cdc1df69409ebfd8ecdc345c6dcacea0d2d1c2c
Adam Drew <adamrdrew@live.com>
2018-02-03 00:04:15 -0500

    Main vue app scaffolding

--- public/main.js
+++ public/main.js
@@ -1,5 +1,30 @@
+var routes = [
+    {name: "login", path: '/login', component: login_form},
+    {path: "/", redirect: "/search"}
+]
+
 var router = new VueRouter({
     routes: routes,
     mode: 'history'
 })
 
+var store = new Vuex.Store({
+    state: {
+        data: [],
+    },
+    getters: {
+    },
+    mutations: {
+    }
+});
+
+var app = new Vue({
+    el: "#vue_app",
+    router: router,
+    store: store, 
+    data: {
+    },
+    methods: {
+
+     }
+});
