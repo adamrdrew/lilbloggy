86c3aaae4e5c853a30a5bde473d609beb46e2532
Adam Drew <adamrdrew@live.com>
2018-02-06 21:04:21 -0500

    Login form is working

--- vue/login_form.vue
+++ vue/login_form.vue
@@ -1,29 +1,29 @@
 <component name="login_form">
 <template lang="slim">
-.col-md-4.col-md-offset-4
-    .panel.panel-default
-        .panel-heading.panel-heading-pink
-            h3.panel-title Log In
-        .panel-body
+.col-md-4.offset-md-4
+    .card.lb-post.w-100.mb-3
+        .card-header
+            h4.card-title.lb-title Log In
+        .card-body
             .form-group
                 label User Name
                 input.form-control placeholder="cool_user_1982" type="text" v-model="name"
-                .alert.alert-danger v-if="!name_is_valid"  That\'s not a valid username.
+                .alert.alert-danger v-if="!name_is_valid"  That is not a valid username.
             .form-group
                 label Password
                 input.form-control v-model="password" @keyup.enter="submit"  type="password"
-        .panel-footer.clearfix
             button.btn.btn-success.pull-right v-bind:disabled="!can_submit" v-on:click="submit"  Log In
 </template>
 
 <script>
     data: function() {
-        return {name: "",
-                password: "",
-                salt: "",
-                hash: "",
-                uuid: ""
-                }
+        return {
+            name: "",
+            password: "",
+            salt: "",
+            hash: "",
+            uuid: ""
+        }
     },
     methods: {
         submit: function() {
@@ -33,23 +33,13 @@
 
         },
         get_salt_and_uuid: function() {
-            $.ajax({
+            XHR({
                 method: 'POST',
-                url: '/api/auth_handshake',
-                dataType : "json",
-                contentType: "application/json; charset=utf-8",
-                context: this,
-                data: JSON.stringify({name: this.name}),
-                cache: false,
-                success: function(result) {
-                    this.salt = result.salt;
-                    this.perform_auth();
-                },
-                error: function(result) {
-                    this.$dispatch('login', false);
-                    alert('Username or password not recognized');
-                }
-            });
+                url: "/auth/handshake"
+            }).success(function (data) {
+                this.salt = data.salt;
+                this.perform_auth();
+            }.bind(this));
         },
         perform_auth: function() {
             var auth_payload = this.auth_payload;
@@ -74,20 +64,20 @@
         }
     },
     computed: {
-        'name_is_valid': function() {
+        name_is_valid: function() {
             return !/[^a-zA-Z0-9\-\\_\/]/.test(this.name);
         },
-        'pw_is_valid': function() {
+        pw_is_valid: function() {
             if (this.password.length < 8 ) {
                 return false;
             };
             return true;
         },
-        'can_submit': function() {
+        can_submit: function() {
             if (this.name_is_valid && this.pw_is_valid) return true;
             return false;
         },
-        'auth_payload': function() {
+        auth_payload: function() {
             var hasher = new jsSHA("SHA-512", "TEXT");
             hasher.update(this.password+this.salt);
             this.hash = hasher.getHash("HEX");
