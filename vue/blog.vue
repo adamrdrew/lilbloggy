09d66cdb0850dd31a017d1de5552d32b0b894c85
Adam Drew <adamrdrew@live.com>
2018-02-06 21:05:51 -0500

    Rough draft of the Blog component is working

--- vue/blog.vue
+++ vue/blog.vue
@@ -3,37 +3,78 @@
 .container-fluid
     .row
         .col-md-7.offset-md-1
-            post v-for="post in posts" :post="post" :key="post.id"
+            post :post="post"
+            router-link.card-link.lb-link v-if="can_go_back"    v-bind:to="post_url(prev_id)" Older
+            router-link.card-link.lb-link v-if="can_go_forward" v-bind:to="post_url(next_id)" Newer
         .col-md-3
             .card.position-fixed
                 .card-body 
-                    p Hello World
+                    p This iwll be the menu
 </template>
 
 <script>
 data: function() {
     return {
-        posts: [
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122},
-            {title: "This is a post", body: "This is a super great post!", id:123, image: "http://4.bp.blogspot.com/-dsGVHCka_Vw/T9tG6vYsh2I/AAAAAAAAAkM/ztGXwGbApkA/s1600/david-bowie-001.jpg"},
-            {title: "This is a an olderpost", body: "This was a super great post!", id:122}
-        ]    
+        post: {
+            title: "", 
+            body:  "", 
+            id:    0, 
+            image: ""
+        },
+        first: 0,
+        last:  0
     }
 },
 methods: {
+    get_id_range: function() {
+        XHR({
+            method: 'GET',
+            url: "/post/range"
+        }).success(function (data) {
+            this.first = data.first;
+            this.last  = data.last;
+        }.bind(this));
+    },
+    get_post: function() {
+        XHR({
+            method: 'GET',
+            url: "/post/" + this.$route.params.postid
+        }).success(function (data) {
+            this.post = data;
+        }.bind(this));
+    },
+    get_latest_post: function() {
+        XHR({
+            method: 'GET',
+            url: "/post/latest"
+        }).success(function (data) {
+            this.post = data;
+            router.push({ name: 'blog', params: { postid: data.id }})
+        }.bind(this));
+    },
+    post_url: function(id) {
+        return "/blog/" + id
+    }
 },
 computed: {
+    can_go_back: function() { return this.post.id > this.first },
+    can_go_forward: function() { return this.post.id < this.last },
+    next_id: function() { a = parseInt(this.post.id); return a += 1 },
+    prev_id: function() { a = parseInt(this.post.id); return a -= 1},
+    id_in_url: function() { return this.$route.params.postid}
+},
+watch: {
+    id_in_url: function() {
+        this.get_post();
+    }
+},
+mounted: function() {
+    this.get_id_range();
+    if (!this.id_in_url) {
+        this.get_latest_post();
+    } else {
+        this.get_post();
+    }
 }
 </script>
 
