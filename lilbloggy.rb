e1d050bfca9649e7bb0bc31934825143657ba004
Adam Drew <adamrdrew@live.com>
2018-02-06 21:08:30 -0500

    Sinatra routes are coming together. Getting posts works, as does new user signup, name validation, and login

--- lilbloggy.rb
+++ lilbloggy.rb
@@ -5,6 +5,9 @@ require 'slim'
 require 'sequel'
 require 'sqlite3'
 require 'securerandom'
+require 'json'
+require './config/config'
+require './lib/exceptions'
 
 DB = Sequel.sqlite('data/lilbloggy.db')
 require './lib/blog'
@@ -15,6 +18,10 @@ require './lib/user'
 
 class LilBloggy < Sinatra::Base
     helpers Sinatra::Cookies
+
+    get '/config' do
+        return json Config::APPCONFIG
+    end
     
     get '/api/get_user' do
         raise NotAuthorizedException unless cookies[:uuid]
@@ -32,7 +39,7 @@ class LilBloggy < Sinatra::Base
         return json({success: true})
     end
 
-    post'/api/auth_handshake' do
+    post'/auth/handshake' do
         name = JSON.parse(request.body.read)['name']
         auth_deets = AuthToken.get_user_salt(name)
         return json auth_deets
@@ -46,13 +53,15 @@ class LilBloggy < Sinatra::Base
         return json({uuid: token['uuid']})
     end
 
-    post '/api/name_available' do
+    post '/user/check_name' do
+        raise SignupDisabledException unless Config::APPCONFIG[:signup_enabled]
         name = JSON.parse(request.body.read)['name']
         response = {taken: AuthToken.user_exists?(name)}
         return json response
     end
 
-    post '/api/signup' do
+    post '/user/signup' do
+        raise SignupDisabledException unless Config::APPCONFIG[:signup_enabled]
         j = JSON.parse(request.body.read)
         token = AuthToken.new_token(j['name'], j['salt'], j['hash'])
         cookies[:uuid] = token[:uuid]
@@ -62,6 +71,24 @@ class LilBloggy < Sinatra::Base
         new_user.uuid = cookies[:uuid]
         new_user.save
         return json({:auth => true})
+    end
+
+    get '/post/range' do
+        first = Post.first.id
+        last  = Post.last.id
+        ret = {first: first, last: last}
+        return ret.to_json
+    end
+
+    get '/post/latest' do
+        post  = Post.last
+        return post.to_hash.to_json
+    end
+
+    get '/post/:id' do
+        id = params['id']
+        post = Post.where(id: id).first
+        return post.to_hash.to_json
     end
 
     get "/*" do
